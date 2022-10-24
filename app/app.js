ec5ad65307ad75607cc7ba75caa3b7d68b8fcb1a
avihavai <avihavai@users.noreply.github.com>
2022-10-24 08:54:31 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.160.0/http/server.ts";
 
 const handleRequest = (request) => {
   console.log(`Request to ${request.url}`);
