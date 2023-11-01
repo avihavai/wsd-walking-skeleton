ffa0167783da8b3655a3294d3b8346c161303e48
avihavai <avihavai@users.noreply.github.com>
2023-11-01 18:25:20 +0200

    Update deps.js

--- app/deps.js
+++ app/deps.js
@@ -1,3 +1,3 @@
 export { serve } from "https://deno.land/std@0.202.0/http/server.ts";
-import postgres from "https://deno.land/x/postgresjs@v3.3.5/mod.js";
+import postgres from "https://deno.land/x/postgresjs@v3.4.2/mod.js";
 export { postgres };
